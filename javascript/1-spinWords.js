47bb777e1ac34c81131747b8382c8d99f49e593e
Andriw Fonseca <filipefonsequinha@gmail.com>
2020-09-30 13:30:06 -0300

    criação do repositorio

---
+++ javascript/1-spinWords.js
@@ -0,0 +1,10 @@
+/*Escreva uma função que receba uma string de uma
+ ou mais palavras e retorne a mesma string, mas com
+  todas as palavras de cinco ou mais letras invertidas
+   (exemplo: palavra de cinco letras e não menos)
+   resultado: arvalap de ocnic sartel e não sonem */
+
+function spinWords(palavra){    
+    let palavraSeparada = palavra.split(" ");
+    return palavraSeparada
+}
